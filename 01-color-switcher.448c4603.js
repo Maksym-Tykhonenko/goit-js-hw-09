8a6016f112a6a093e64e82c669df15741d37860d
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2022-08-14 17:35:50 +0000

    Deploying to gh-pages from @ Maksym-Tykhonenko/goit-js-hw-09@8c5180b767ae2d6e96eb85da5e7c9a2e24ec2ceb 🚀

---
+++ 01-color-switcher.448c4603.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.btnStart.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));let e=null;
+//# sourceMappingURL=01-color-switcher.448c4603.js.map
