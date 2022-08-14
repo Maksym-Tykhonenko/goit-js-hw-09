8c5180b767ae2d6e96eb85da5e7c9a2e24ec2ceb
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2022-08-14 20:35:21 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+const TIME_COL_DALEY = 1000;
 
+refs.btnStart.addEventListener('click', onChangeCol);
+refs.btnStop.addEventListener('click', stopChangeCol);
+
+let intervalId = null;
+
+function onChangeCol() {
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, TIME_COL_DALEY);
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+};
+
+
+function stopChangeCol(){
+    clearInterval(intervalId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
