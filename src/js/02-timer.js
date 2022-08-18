589d70c4a3d3c90ab4ed58e92fa7ee4e6eaff8de
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2022-08-19 00:33:30 +0300

    last com

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,41 +1,38 @@
 import flatpickr from "flatpickr";
+import Notiflix from 'notiflix';
 import "flatpickr/dist/flatpickr.min.css";
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const refs = {
     input: document.querySelector('#datetime-picker'),
     countdownBtn: document.querySelector('[data-start]'),
     timer: document.querySelector('.timer'),
+    daysCounter: document.querySelector('[data-days]'),
+    hoursCounter: document.querySelector('[data-hours]'),
+    minutesCounter: document.querySelector('[data-minutes]'),
+    secondsCounter: document.querySelector('[data-seconds]'),
 }
-flatpickr(refs.input, options);
-refs.input.addEventListener('click', onSelektDate);
-refs.countdownBtn.addEventListener('click', onStartCountdown);
 
-
+refs.countdownBtn.disabled = true;
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-}
-
-
-
-function onSelektDate() {
-    
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] < new Date()) {
+            Notiflix.Notify.warning('Please choose a date in the future', {
+                position: 'center-top',
+            });
+            refs.countdownBtn.disabled = true;
+            return;
+        }
+        refs.countdownBtn.disabled = false;
+    },
 };
-    
-function onStartCountdown() {
-    
-};
+flatpickr(refs.input, options);
 
-
-
-
-//////////////////////////////////////////////////////////
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -54,11 +51,33 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-//console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 
-//console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+   
+const onStartCountdown = () => {
+    const timerId = setInterval(() => {
+        const countdown = new Date(refs.input.value) - new Date();
+        refs.countdownBtn.disabled = true;
+        if (countdown >= 0) {
+            refs.input.disabled = true;
+            const timeObject = convertMs(countdown);
+            refs.daysCounter.textContent = addLeadingZero(timeObject.days);
+            refs.hoursCounter.textContent = addLeadingZero(timeObject.hours);
+            refs.minutesCounter.textContent = addLeadingZero(timeObject.minutes);
+            refs.secondsCounter.textContent = addLeadingZero(timeObject.seconds);
+        } else {
+            clearInterval(timerId);
+            Notiflix.Notify.success('Finish!', {
+                position: 'left-top',
+            });
+            refs.input.disabled = false;
+        }
+    }, 1000)
+};
 
+refs.countdownBtn.addEventListener('click', onStartCountdown);
 
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+
+let delay = document.querySelector('[name="delay"]');
+let step = document.querySelector('[name="step"]');
+let amount = document.querySelector('[name="amount"]');
+const button = document.querySelector('button');
+
+button.addEventListener('click', dataForPromice);
+
+function dataForPromice(event) {
+  event.preventDefault();
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+
+  if (firstDelay < 0 || delayStep < 0 || amount.value < 0) {
+    Notiflix.Report.warning(
+      'Негативне значення',
+      'Введіть позитивне ',
+      'ЗРОЗУМІЛО'
+    );
   } else {
-    // Reject
+    for (let i = 0; i < amount.value; i++) {
+      let nextTime = firstDelay + i * delayStep;
+      createPromise(1 + i, nextTime)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
